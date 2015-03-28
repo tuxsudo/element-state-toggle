d3209b28f9bb0ee0aa5479fb0caf7e99e01eda3f
Jared Anderson <jared@tuxsudo.com>
2015-03-28 13:15:10 -0600

    now with some documentation

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Element State Toggle
 
-An event-driven low-level component to toggle DOM elements' state
+A light-weight, event-driven, low-level component to toggle DOM elements' state. 
+
+Zero dependencies!
+
+
+## Usage
+
+
+### Commands (to toggle a the state)
+
+
+```
+var element = document.querySelector('.element');
+
+element.dispatchEvent(new CustomEvent('cmd.state.toggle'));
+element.dispatchEvent(new CustomEvent('cmd.state.on'));
+element.dispatchEvent(new CustomEvent('cmd.state.off'));
+
+```
+
+
+### Notifications (when element is toggled)
+
+
+```
+var element = document.querySelector('.element');
+
+element.addEventListener('state.on', function(e) {
+    console.log('state %s was turned on', e.detail); 
+});
+
+element.addEventListener('state.off', function(e) {
+    console.log('state %s was turned off', e.detail); 
+});
+```
+
+### Support
+
+IE9+ -- [Polyfill](https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill) for CustomEvent constructor required.
--- src/lib.js
+++ src/lib.js
@@ -8,32 +8,32 @@ Polyfill Required: `CustomEvent` polyfill (IE9+)
 // See defaults.js for structure of config
 export default function ( config ) {
 
-        // attribute to toggle passed by e.detail (or default value)
-    var getattr = e => e.detail || config.attribute,
+		// attribute to toggle passed by e.detail (or default value)
+	var getattr = e => e.detail || config.attribute,
 
-        // shortcut to dispatch bubbling events
-        dispatch = (elm, ev, data) => elm.dispatchEvent( new CustomEvent(ev, { bubbles: true, detail: data }) ),
+		// shortcut to dispatch bubbling events
+		dispatch = (elm, ev, data) => elm.dispatchEvent( new CustomEvent(ev, { bubbles: true, detail: data }) ),
 
-        // add attribute to emitting element (e.target)
-        on = e => {
+		// add attribute to emitting element (e.target)
+		on = e => {
 			var attribute = getattr(e);
-        	e.target.setAttribute( attribute, 1 );
-        	dispatch(e.target, config.notices.on, attribute);
-        },
+			e.target.setAttribute( attribute, 1 );
+			dispatch(e.target, config.notices.on, attribute);
+		},
 
-        // remove attribute from emitting element (e.target)
-        off = e => {
-        	var attribute = getattr(e);
-        	e.target.removeAttribute( attribute );
-        	dispatch(e.target, config.notices.on, attribute);
-        },
+		// remove attribute from emitting element (e.target)
+		off = e => {
+			var attribute = getattr(e);
+			e.target.removeAttribute( attribute );
+			dispatch(e.target, config.notices.on, attribute);
+		},
 
-        // toggle the attribute
-        toggle = e => e.target.hasAttribute( getattr(e) ) ? off(e) : on(e);
+		// toggle the attribute
+		toggle = e => e.target.hasAttribute( getattr(e) ) ? off(e) : on(e);
 
-    // delegate event listening to the window
-    window.addEventListener(config.commands.on, on);
-    window.addEventListener(config.commands.off, off);
-    window.addEventListener(config.commands.toggle, toggle);
+	// delegate event listening to the window
+	window.addEventListener(config.commands.on, on);
+	window.addEventListener(config.commands.off, off);
+	window.addEventListener(config.commands.toggle, toggle);
 
 }
